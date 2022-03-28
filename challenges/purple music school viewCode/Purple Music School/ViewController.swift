f413ee0f8d30c2190e8cc1c4b311e98a671944a1
codecampos <matheuscp81@gmail.com>
2022-03-27 23:47:52 -0300

    update purple music school viewCode app

--- challenges/purple music school viewCode/Purple Music School/ViewController.swift
+++ challenges/purple music school viewCode/Purple Music School/ViewController.swift
@@ -25,7 +25,6 @@ class ViewController: UIViewController {
         stack.distribution = .fill
         stack.spacing = 0
         stack.contentMode = .scaleToFill
-        stack.spacing = 10
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
@@ -34,11 +33,22 @@ class ViewController: UIViewController {
         let stack = UIStackView()
         stack.axis = .vertical
         stack.backgroundColor = .white
-        stack.alignment = .center
+        stack.alignment = .fill
         stack.distribution = .fillEqually
+        stack.spacing = 5
+        stack.contentMode = .scaleToFill
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    lazy var contentHorizontalStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.backgroundColor = .white
+        stack.alignment = .fill
+        stack.distribution = .equalSpacing
         stack.spacing = 0
         stack.contentMode = .scaleToFill
-        stack.spacing = 10
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
@@ -46,8 +56,9 @@ class ViewController: UIViewController {
     lazy var logoTitleLabel: UILabel = {
         let label = UILabel()
         label.textColor = .black
+        label.textAlignment = .justified
         label.text = "Boas Vindas!"
-        label.font = UIFont.systemFont(ofSize: 14)
+        label.font = UIFont.systemFont(ofSize: 24)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -55,8 +66,9 @@ class ViewController: UIViewController {
     lazy var secondLogoTitleLabel: UILabel = {
         let label = UILabel()
         label.textColor = .black
+        label.textAlignment = .justified
         label.text = "Vamos fazer música juntos"
-        label.font = UIFont.systemFont(ofSize: 14)
+        label.font = UIFont.systemFont(ofSize: 24)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -65,7 +77,7 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.textColor = .black
         label.text = "E-mail"
-        label.font = UIFont.systemFont(ofSize: 14)
+        label.font = UIFont.systemFont(ofSize: 17)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -74,7 +86,7 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.textColor = .black
         label.text = "Senha"
-        label.font = UIFont.systemFont(ofSize: 14)
+        label.font = UIFont.systemFont(ofSize: 17)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -84,10 +96,11 @@ class ViewController: UIViewController {
         email.tintColor = .systemGray
         email.placeholder = "Informe seu e-mail"
         email.font = UIFont.systemFont(ofSize: 14)
+        email.borderStyle = .roundedRect
         email.textContentType = .emailAddress
         email.autocorrectionType = .no
         email.translatesAutoresizingMaskIntoConstraints = false
-        return emailTextField
+        return email
     }()
     
     lazy var passwordTextField: UITextField = {
@@ -95,10 +108,11 @@ class ViewController: UIViewController {
         password.tintColor = .systemGray
         password.placeholder = "Informe sua senha"
         password.font = UIFont.systemFont(ofSize: 14)
+        password.borderStyle = .roundedRect
         password.textContentType = .password
         password.isSecureTextEntry = true
         password.translatesAutoresizingMaskIntoConstraints = false
-        return emailTextField
+        return password
     }()
     
     lazy var autenticatorButton: UIButton = {
@@ -106,13 +120,40 @@ class ViewController: UIViewController {
         
         button.tintColor = .white
         button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemPurple
+        button.backgroundColor = UIColor(red: 0.443, green: 0.345, blue: 0.657, alpha: 1.0)
         button.setTitle("Entrar", for: .normal)
+        button.layer.cornerRadius = 5
         button.addTarget(self, action: #selector(autenticator), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
+    lazy var resetPasswordButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(UIColor(red: 0.443, green: 0.345, blue: 0.657, alpha: 1.0), for: .normal)
+        button.setTitle("Esqueci minha senha", for: .normal)
+        button.titleLabel?.font =  UIFont.systemFont(ofSize: 15)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var signUpButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(UIColor(red: 0.443, green: 0.345, blue: 0.657, alpha: 1.0), for: .normal)
+        button.setTitle("Não tem uma conta?", for: .normal)
+        button.titleLabel?.font =  UIFont.systemFont(ofSize: 15)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var helpButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(UIColor(red: 0.443, green: 0.345, blue: 0.657, alpha: 1.0), for: .normal)
+        button.setTitle("Ajuda", for: .normal)
+        button.titleLabel?.font =  UIFont.systemFont(ofSize: 15)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     @objc private func autenticator() {
         if let email = emailTextField.text, let password = passwordTextField.text {
@@ -160,7 +201,7 @@ class ViewController: UIViewController {
         view.addSubview(contentVerticalStack)
         
         NSLayoutConstraint.activate([
-            contentVerticalStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            contentVerticalStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             contentVerticalStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             contentVerticalStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
@@ -168,7 +209,7 @@ class ViewController: UIViewController {
         
         contentVerticalStack.addArrangedSubview(logoImageView)
         NSLayoutConstraint.activate([
-            logoTitleLabel.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
         ])
         
         contentVerticalStack.addArrangedSubview(logoTitleLabel)
@@ -177,14 +218,44 @@ class ViewController: UIViewController {
         view.addSubview(secondContentVerticalStack)
         
         NSLayoutConstraint.activate([
-            secondContentVerticalStack.topAnchor.constraint(equalTo: contentVerticalStack.bottomAnchor, constant: 70),
+            secondContentVerticalStack.topAnchor.constraint(equalTo: contentVerticalStack.bottomAnchor, constant: 16),
             secondContentVerticalStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             //secondContentVerticalStack.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 16),
             secondContentVerticalStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
         
         secondContentVerticalStack.addArrangedSubview(emailTitleLabel)
-        //contentVerticalStack.addArrangedSubview(autenticatorButton)
+        secondContentVerticalStack.addArrangedSubview(emailTextField)
+        secondContentVerticalStack.addArrangedSubview(passwordTitleLabel)
+        secondContentVerticalStack.addArrangedSubview(passwordTextField)
+        
+        view.addSubview(autenticatorButton)
+        
+        NSLayoutConstraint.activate([
+            autenticatorButton.topAnchor.constraint(equalTo: secondContentVerticalStack.bottomAnchor, constant: 16),
+            autenticatorButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            autenticatorButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
+        view.addSubview(resetPasswordButton)
+        
+        NSLayoutConstraint.activate([
+            resetPasswordButton.topAnchor.constraint(equalTo: autenticatorButton.bottomAnchor, constant: 30),
+            resetPasswordButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            resetPasswordButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
+        view.addSubview(contentHorizontalStack)
+        
+        NSLayoutConstraint.activate([
+            contentHorizontalStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            contentHorizontalStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 10),
+            contentHorizontalStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
+        contentHorizontalStack.addArrangedSubview(signUpButton)
+        contentHorizontalStack.addArrangedSubview(helpButton)
+        
     }
 }
 
